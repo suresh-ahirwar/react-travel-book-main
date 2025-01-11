807eaf92584c10d96dbaf69fde12076d1c88475c
suresh ahirwar <ahirwarsuresh077@gmail.com>
2025-01-11 13:58:19 +0530

    first commit travel book project

---
+++ src/components/Map.jsx
@@ -0,0 +1,6 @@
+import style from "../styles/map.module.css";
+function Map() {
+    return <div className={style.map}>map</div>;
+}
+
+export default Map;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,10 @@
+import style from "../styles/sideBar.module.css"
+function SideBar() {
+    return (
+        <div className={style.sideBar}>
+            SideBar
+        </div>
+    )
+}
+
+export default SideBar
